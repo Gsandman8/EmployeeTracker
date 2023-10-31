d596f1972707e0ac21023f5e0af1c032b5b7f567
Gsandman8 <gage.e.sandman@gmail.com>
2023-10-31 13:35:36 -0700

    Finished added relevant inquirer prompts and fixed queries to match database sematics

--- controllers/queries.js
+++ controllers/queries.js
@@ -1,34 +1,35 @@
 class Calls{
-    getDepartmentsArray(){
-        return `SELECT name FROM department WHERE id = ${id}`;
-    }
     getDepartments(){
-        return "SELECT * FROM departments";
+        return "SELECT * FROM department";
     }
 
     getRoles(){
-        return "SELECT * FROM roles LEFT JOIN departments ON roles.department_id = departments.id";
+        return "SELECT * FROM role LEFT JOIN department ON role.department_id = department.id";
     }
 
     getEmployees(){
-        return "SELECT * FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id";
+        return "SELECT * FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id";
+    }
+
+    getManagers(){
+        return "SELECT * FROM employee WHERE manager_id IS NULL";
     }
 
     addDepartment(name){     
-        return `INSERT INTO departments (name) VALUES ('${name}')`;
+        return `INSERT INTO department (name) VALUES ('${name}')`;
     }
 
     addRole(title, salary, department_id){
-        return `INSERT INTO roles (title, salary, department_id) VALUES ('${title}', ${salary}, ${department_id})`;
+        return `INSERT INTO role (title, salary, department_id) VALUES ('${title}', ${salary}, ${department_id})`;
     }
 
     addEmployee(first_name, last_name, role_id, manager_id){
-        return `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('${first_name}', '${last_name}', ${role_id}, ${manager_id})`;
+        return `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${first_name}', '${last_name}', ${role_id}, ${manager_id})`;
     }
 
     UpdateEmployeeRole(role_id, employee_id){
-        return `UPDATE employees SET role_id = ${role_id} WHERE id = ${employee_id}`;
+        return `UPDATE employee SET role_id = ${role_id} WHERE id = ${employee_id}`;
     }
 }
 
-module.exports = Calls();
+module.exports = Calls;
--- index.js
+++ index.js
@@ -1,12 +1,64 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const db = require('./config/connection');
-const query = require('./db/queries');
+const Calls = require('./controllers/queries');
+const Query = new Calls(); 
 
 const departments = function() {
     const departments = [];
+    db.query(Query.getDepartments(), (err, res) => {
+        if (err) {
+            console.log(err);
+        }
+        res.forEach(department => {
+            departments.push(department.name);
+        });
+    });
+    return departments;
 };
+const departmentList = departments();
 
+const roles = function() {
+    const roles = [];
+    db.query(Query.getRoles(), (err, res) => {
+        if (err) {
+            console.log(err);
+        }
+        res.forEach(role => {
+            roles.push(role.title);
+        });
+    });
+    return roles;
+}
+const rolesList = roles();
+
+const employees = function() {
+    const employees = [];
+    db.query(Query.getEmployees(), (err, res) => {
+        if (err) {
+            console.log(err);
+        }
+        res.forEach(employee => {
+            employees.push(employee.first_name + " " + employee.last_name);
+        });
+    });
+    return employees;
+}
+const employeesList = employees();
+
+const managers = function() { 
+    const managers = [];
+    db.query(Query.getManagers(), (err, res) => {
+        if (err) {
+            console.log(err);
+        }
+        res.forEach(manager => {
+            managers.push(manager.first_name + " " + manager.last_name);
+        });
+    });
+    return managers;
+}
+const managersList = managers();
 
 const mainMenu = {
     type: 'list',
@@ -28,23 +80,61 @@ const addDepartment = {
     message: 'What is the name of the department you would like to add?'
 };
 
-const addRole = {
-    type: 'input',
-    name: 'addRole',
-    message: 'What is the title of the role you would like to add?'
-}
+const addRole = [
+    {
+        type: 'input',
+        name: 'addRole',
+        message: 'What is the title of the role you would like to add?'
+    },
+    {
+        type: 'input',
+        name: 'addSalary',
+        message: 'What is the salary of the role you would like to add?'
+    },
+    {
+        type: 'list',
+        name: 'addDepartmentId',
+        message: `What is the department you would to add this role to?`,
+        choices: [...departmentList]
+    }
+];
 
-const addSalary = {
-    type: 'input',
-    name: 'addSalary',
-    message: 'What is the salary of the role you would like to add?'
-}
+const addEmployee = [
+    {
+        type: 'input',
+        name: 'firstName',
+        message: `What is the employee's first name?`
+    },
+    {
+        type: 'input',
+        name: 'lastName',
+        message: `What is the employee's last name?`
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: `What is the employee's role?`,
+        choices: [...rolesList]
+    },
+    {
+        type: 'list',
+        name: 'manager',
+        message: `Who is the employee's manager?`,
+        choices: [...managersList, "No Manager"]
+    }
+];
 
-const addDepartmentId = {
-    type: 'list',
-    name: 'addDepartmentId',
-    message: `What is the department you would to add this role to?`,
-    choices: [
-        
-    ]
-}
+const UpdateEmployeeRole = [
+    {
+        type: 'list',
+        name: 'employee',
+        message: `Which employee would you like to update?`,
+        choices: [...employeesList]
+    },
+    {
+        type: 'list',
+        name: 'newRole',
+        message: `What is the employee's new role?`,
+        choices: [...rolesList]
+    }
+];
